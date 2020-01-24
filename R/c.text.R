df5b786db9800081a62005e9180de284f3f19554
jhellewell14 <Joel.Hellewell@lshtm.ac.uk>
2020-01-24 09:14:20 +0000

    No longer need c.text to be an S3 method. Changed to c_text

--- R/c.text.R
+++ R/c.text.R
@@ -8,7 +8,8 @@
 #'
 #' @examples
 #'
-c.text<-function(x,sigF=3){
+c_text<-function(x,sigF=3){
   bp1=signif(c(median(x),quantile(x,0.025),quantile(x,0.975)),sigF)
   paste(bp1[1]," (",bp1[2],"-",bp1[3],")",sep="")
 }
+
