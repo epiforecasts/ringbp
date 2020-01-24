687fc4f56032727890502d4714c7c8bee41ea6f9
jhellewell14 <Joel.Hellewell@lshtm.ac.uk>
2020-01-24 10:11:06 +0000

    New function to calculate proportion of simulation runs that controlled outbreak. Currently an arbitrary definition of no cases between weeks 10-12 after outbreak

---
+++ R/extinct_prob.R
@@ -0,0 +1,18 @@
+#' Calculate proportion of runs that have controlled outbreak
+#'
+#' @param outbreak_df_week
+#' @author Joel Hellewell
+#' @return
+#' @export
+#'
+#' @examples
+#' @importFrom dplyr group_by filter summarise
+extinct_prob <- function(outbreak_df_week){
+  n.sim = max(outbreak_df_week$n.sim)
+  out <- outbreak_df_week %>%
+    group_by(n.sim) %>% # group by simulation run
+    filter(week %in% c(10,11,12)) %>% # filter to weeks 10-12
+    summarise(extinct=ifelse(all(number==0),1,0)) %>% # new variable extinct = 1 if cases in weeks 10-12 all 0, 0 if not
+    .$extinct %>% sum(.)/n.sim # number of runs where extinct = TRUE / number of runs
+  return(out)
+}
