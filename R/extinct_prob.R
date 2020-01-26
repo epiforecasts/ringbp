f7f9051585dd98e90041e0831c877a57fa25d313
Joel Hellewell <Joel.Hellewell@lshtm.ac.uk>
2020-01-26 15:18:11 +0000

    Extended extinction function

--- R/extinct_prob.R
+++ R/extinct_prob.R
@@ -7,12 +7,13 @@
 #'
 #' @examples
 #' @importFrom dplyr group_by filter summarise
-extinct_prob <- function(outbreak_df_week){
+extinct_prob <- function(outbreak_df_week,cap_cases){
   n.sim = max(outbreak_df_week$n.sim)
   out <- outbreak_df_week %>%
     group_by(n.sim) %>% # group by simulation run
+    mutate(cs=cumsum(number)) %>%
     filter(week %in% 12:16) %>%
-    summarise(extinct=ifelse(all(number==0),1,0)) %>% # new variable extinct = 1 if cases in weeks 10-12 all 0, 0 if not
+    summarise(extinct=ifelse(all(number==0 & cs < cap_cases),1,0)) %>% # new variable extinct = 1 if cases in weeks 10-12 all 0, 0 if not
     .$extinct %>% sum(.)/n.sim # number of runs where extinct = TRUE / number of runs
   return(out)
 }
