8f74e0754cf36c06f047f03d634a7ebebd7fb488
Joel Hellewell <Joel.Hellewell@lshtm.ac.uk>
2020-01-27 19:17:00 +0000

    Separating new functions into separate files, writing documentation

---
+++ R/new_cluster.R
@@ -0,0 +1,44 @@
+#' Draw new cluster cases from index case
+#' @author Joel Hellewell
+#' @param index_case Index case of cluster
+#' @param r0community basic reproduction number for non-isolated cases
+#' @param disp.com dispersion parameter for negative binomial distribution for non-isolated cases
+#' @param total.clusters current number of clusters
+#'
+#' @return
+#' @export
+#' @importFrom dplyr mutate bind_rows
+#' @examples
+new_cluster <- function(index_case, r0community, disp.com, total.clusters) {
+
+  if (index_case$cluster < total.clusters) {
+    return(index_case) # filters out old clusters and returns them unchanged
+  }
+
+  index_case$isolated <- TRUE # this case has had a chance to infect, so now it is isolated
+
+  new_cases <- rnbinom(1, size = disp.com, mu = r0community) # number of new cases in the cluster formed from missed case
+
+  if (new_cases == 0) {
+    return(index_case)
+  }else{
+    cluster_data <-
+      data.frame(
+        cluster = rep(index_case$cluster, new_cases),
+        # set cluster number to index case cluster number
+        onset = index_case$latent + incubfn(new_cases),
+        # onset time from when infector infected + incubation period
+        exposure = rep(index_case$latent, new_cases),
+        # exposure time is when infector infected
+        latent = index_case$latent + infecfn(new_cases),
+        # time when these cases will infect
+        missed = rep(FALSE, new_cases)
+      ) %>% # none missed since they are all in this new cluster
+
+      dplyr::mutate(isolated_time = min(onset) + rep_fn(1), # cluster is isolated at minimum onset time + delay
+             isolated = ifelse(latent > isolated_time, TRUE, FALSE)) %>% # cases that don't infect before isolation are marked isolated
+      dplyr::bind_rows(index_case) # add index case of cluster back to dataset
+
+    return(cluster_data)
+  }
+}
--- R/outbreak_model.R
+++ R/outbreak_model.R
@@ -24,98 +24,18 @@ infecfn <- function(x) {
 
 rep_fn <- function(x) {
   delay_param = c(3, 1)
-
-  rgamma(
-    x,
+  rgamma(x,
     shape = delay_param[1] / (delay_param[2] ^ 2 / delay_param[1]),
-    scale = (delay_param[2] ^ 2 / delay_param[1])
-  )
-
+    scale = (delay_param[2] ^ 2 / delay_param[1]))
 }
 
-
-secondary_draw <- function(index_case, r0isolated, r0community, disp.iso, disp.com,prop.ascertain) {
-
-  vect_isTRUE <- function(x) {
-    purrr::map_lgl(x, isTRUE)
-  }
-
-  index_case <- dplyr::mutate(index_case,
-                              new.cases = purrr::map2_dbl(
-                                ifelse(vect_isTRUE(isolated), disp.iso, disp.com),
-                                ifelse(vect_isTRUE(isolated), r0isolated, r0community),
-                                ~ rnbinom(1, size = .x, mu = .y)),isolated = TRUE)
-
-
-  new_cases <- dplyr::filter(index_case, new.cases > 0)
-
-
-  if (nrow(new_cases) > 0) {
-    new_cases <- dplyr::group_by(new_cases, caseid)
-
-    new_cases <- dplyr::group_split(new_cases)
-
-    new_cases <- map(new_cases,~mutate(data.frame(exposure = rep(.$latent, times = .$new.cases),
-                                                  # exposure is taken from infector data
-                                                  missed = rbernoulli(n = .$new.cases, p = prop.ascertain)),
-                                       ## whether these cases remain in cluster or are missed
-                                       onset = exposure + incubfn(.$new.cases),
-                                       # onset of new cases
-                                       latent = exposure + infecfn(.$new.cases),
-                                       # when new cases infect people
-                                       cluster = ifelse(vect_isTRUE(missed), NA, .$cluster),
-                                       # remain within cluster with prob = prop.ascertain
-                                       # set isolated time from onset, same as infector if you remain within cluster
-                                       isolated_time = ifelse(vect_isTRUE(missed),onset + rep_fn(.$new.cases),
-                                                              .$isolated_time + rep_fn(.$new.cases)),isolated = !vect_isTRUE(missed)))
-
-    index_case <- dplyr::bind_rows(dplyr::select(index_case,-new.cases), new_cases)
-
-  } else{
-    index_case <- dplyr::select(index_case,-new.cases)
-  }
-
-  return(index_case)
-} # End function
-
-new_cluster <- function(index_case, r0community, disp.com, total.clusters) {
-
-  if (index_case$cluster < total.clusters) {
-    return(index_case) # filters out old clusters and returns them unchanged
-  }
-
-  index_case$isolated <- TRUE # this case has had a chance to infect, so now it is isolated
-
-  new_cases <- rnbinom(1, size = disp.com, mu = r0community) # number of new cases in the cluster formed from missed case
-
-  if (new_cases == 0) {
-    return(index_case)
-  }else{
-    cluster_data <-
-      data.frame(
-        cluster = rep(index_case$cluster, new_cases),
-        # set cluster number to index case cluster number
-        onset = index_case$latent + incubfn(new_cases),
-        # onset time from when infector infected + incubation period
-        exposure = rep(index_case$latent, new_cases),
-        # exposure time is when infector infected
-        latent = index_case$latent + infecfn(new_cases),
-        # time when these cases will infect
-        missed = rep(FALSE, new_cases)
-      ) %>% # none missed since they are all in this new cluster
-
-      mutate(isolated_time = min(onset) + rep_fn(1), # cluster is isolated at minimum onset time + delay
-             isolated = ifelse(latent > isolated_time, TRUE, FALSE)) %>% # cases that don't infect before isolation are marked isolated
-      bind_rows(index_case) # add index case of cluster back to dataset
-
-    return(cluster_data)
-  }
+dist_setup <- function(dist_mean,dist_var){
+  dist_param <- c(dist_mean,dist_var)
+  out <- partial(rgamma,
+                 shape = dist_param[1] / (dist_param[2] ^ 2 / dist_param[1]),
+                 scale = (dist_param[2] ^ 2 / dist_param[1]))
+  return(out)
 }
-
-
-
-
-
 
 outbreak_model <- function(num.initial.cases, num.initial.clusters, prop.ascertain,
                            cap_max_days, cap_cases, r0isolated, r0community, disp.iso, disp.com) {
