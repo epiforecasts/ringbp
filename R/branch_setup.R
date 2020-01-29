21cd9641a651534ea0bc93ce22657b590024383e
Joel Hellewell <Joel.Hellewell@lshtm.ac.uk>
2020-01-29 17:16:15 +0000

    Commenting model, tidying repo, add log.txt from parameter sweep to gitignore

--- R/branch_setup.R
+++ R/branch_setup.R
@@ -12,6 +12,7 @@
 #' @importFrom data.table data.table
 #' @examples
 branch_setup <- function(num.initial.cases,num.initial.clusters,incubfn,infecfn,delayfn){
+  # Set up table of initial cases
   case_data <- data.table(exposure = rep(0, num.initial.cases * num.initial.clusters), # Exposure time of 0 for all initial cases
                           onset = incubfn(num.initial.cases * num.initial.clusters), # Draw symptom onset for all initial
                           latent = infecfn(num.initial.cases * num.initial.clusters), # Draw when initial cases infect people
@@ -20,7 +21,10 @@ branch_setup <- function(num.initial.cases,num.initial.clusters,incubfn,infecfn,
                           caseid = 1:(num.initial.cases * num.initial.clusters), # set case id
                           infector = 0)
 
-  case_data <- case_data[,isolated_time := min(onset) + delayfn(1),cluster][,isolated := ifelse(latent > isolated_time, TRUE, FALSE)]
-
+  # set isolation time for cluster to minimum time of onset of symptoms + draw from delay distribution
+  case_data <- case_data[,isolated_time := min(onset) + delayfn(1),cluster
+                         # isolated if time when infecting < isolation time
+                         ][,isolated := ifelse(latent > isolated_time, TRUE, FALSE)]
+  # return
   return(case_data)
 }
