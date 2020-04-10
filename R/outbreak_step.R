be564f65b1007462dcb54bb1fefccf6dfd1725ec
Tim Lucas <timcdlucas@gmail.com>
2020-04-10 14:11:22 +0100

    Move extra functions outside of outbreak step. We dont want to redefine these every simulation.

--- R/outbreak_step.R
+++ R/outbreak_step.R
@@ -34,19 +34,6 @@
 outbreak_step <- function(case_data = NULL, disp.iso = NULL, disp.com = NULL, r0isolated = NULL, r0community = NULL,
                           prop.asym = NULL, incfn = NULL, delayfn = NULL, prop.ascertain = NULL, k = NULL, quarantine = NULL) {
 
-  # A vectorised version of isTRUE
-  vect_isTRUE <- function(x) {
-    purrr::map_lgl(x, isTRUE)
-  }
-
-  vect_max <- function(x, y) {
-    purrr::map2_dbl(x, y, max)
-  }
-
-  vect_min <- function(x, y) {
-    purrr::map2_dbl(x, y, min)
-  }
-
   # For each case in case_data, draw new_cases from a negative binomial distribution
   # with an R0 and dispersion dependent on if isolated=TRUE
   case_data[, new_cases := purrr::map2_dbl(
@@ -154,3 +141,19 @@ outbreak_step <- function(case_data = NULL, disp.iso = NULL, disp.com = NULL, r0
 
   return(out)
 }
+
+
+
+# A vectorised version of isTRUE
+vect_isTRUE <- function(x) {
+  purrr::map_lgl(x, isTRUE)
+}
+
+vect_max <- function(x, y) {
+  purrr::map2_dbl(x, y, max)
+}
+
+vect_min <- function(x, y) {
+  purrr::map2_dbl(x, y, min)
+}
+
