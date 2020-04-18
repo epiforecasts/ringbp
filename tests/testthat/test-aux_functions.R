ef3b02e4b1e8651f91ddd72daecbfe3e107f16f5
Tim Lucas <timcdlucas@gmail.com>
2020-04-18 11:42:43 +0100

    More extinction tests.

--- tests/testthat/test-aux_functions.R
+++ tests/testthat/test-aux_functions.R
@@ -239,24 +239,29 @@ test_that('extinct_prob week_range argument works', {
   
   
   # Case of cases in week 1 but not 2 (by the definition used in this function this is not an extinction)
-  #   test here that week_range 1:2 says extintion by week_range 2 does not.
+  #   test here that week_range 1:2 says no extintion but week_range 2 says extinction.
   res5 <- res[c(1, 2, 3), ]
   res5$weekly_cases[1:3] <- c(1, 1, 0)
   res5$cumulative[1:3] <- c(1, 2, 2)
   r5 <- extinct_prob(res5, cap_cases = cap, week_range = 1:2)
   expect_equal(r5, 0)
   
+  r5b <- extinct_prob(res5, cap_cases = cap, week_range = 2)
+  expect_equal(r5b, 1)
+  
   
   
   # Case of cases in week 2 but not 1 (by all sensible definitions is not an extinction)
-  #   test here that week_range 1:2 says extintion and week_range 2 does as well.
+  #   test here that week_range 1:2 says no extinction and neither does week_range 2.
   res6 <- res[c(1, 2, 3), ]
   res6$weekly_cases[1:3] <- c(1, 0, 1)
   res6$cumulative[1:3] <- c(1, 1, 2)
   r6 <- extinct_prob(res6, cap_cases = cap, week_range = 1:2)
   expect_equal(r6, 0)
   
-    
+  r6b <- extinct_prob(res6, cap_cases = cap, week_range = 2)
+  expect_equal(r6b, 0)
+  
 })
 
 
@@ -352,6 +357,15 @@ test_that('detect_extinct works', {
   expect5 <- tibble::tibble(sim = c(1.0), extinct = c(0.0))
   expect_equal(r5, expect5)
   
+  r5b <- detect_extinct(res5, cap_cases = cap, week_range = 2)
+  # The types in the output is a bit random. So just force all to doubles.
+  r5b <- 
+    r5b %>% 
+    dplyr::mutate_all(as.double)
+  
+  expect5b <- tibble::tibble(sim = c(1.0), extinct = c(1.0))
+  expect_equal(r5b, expect5b)
+  
   
   
   # Case of cases in week 2 but not 1 (by all sensible definitions is not an extinction)
@@ -367,6 +381,16 @@ test_that('detect_extinct works', {
   
   expect6 <- tibble::tibble(sim = c(1.0), extinct = c(0.0))
   expect_equal(r6, expect5)
+  
+  
+  r6b <- detect_extinct(res5, cap_cases = cap, week_range = 2)
+  
+  r6b <- 
+    r6b %>% 
+    dplyr::mutate_all(as.double)
+  
+  expect6b <- tibble::tibble(sim = c(1.0), extinct = c(0.0))
+  expect_equal(r6b, expect5b)
   
   
 })
