9340f315be63723c764fe1cdf14184918da5deb4
Tim Lucas <timcdlucas@gmail.com>
2020-04-18 11:10:53 +0100

    Test extinct_prob carefully. Adds tests for new week_range functionality. Dont want week 12:16 hard coded as the definition of extinction.  But havent expose this argument up the simulation functions yet.

--- tests/testthat/test-aux_functions.R
+++ tests/testthat/test-aux_functions.R
@@ -88,3 +88,139 @@ test_that("inf_fn parameters behave as expected", {
 })
 
 
+test_that('extinct_prob works as expected', {
+  
+  set.seed(12341240)
+  cap <- 100
+  sims <- 5
+  res <- scenario_sim(n.sim = sims,
+                      num.initial.cases = 5,
+                      cap_max_days = 100,
+                      cap_cases = cap,
+                      r0isolated = 0,
+                      r0community = 2.5,
+                      disp.iso = 1,
+                      disp.com = 0.16,
+                      k = 0.7,
+                      delay_shape = 2.5,
+                      delay_scale = 5,
+                      prop.asym = 0,
+                      prop.ascertain = 0)
+  
+  r1 <- extinct_prob(res, cap)
+  
+  expect_true(r1 <= 1)
+  expect_true(r1 >= 0)
+  expect_length(r1, 1)
+  
+  is.wholenumber <- function(x, tol = .Machine$double.eps^0.5)  abs(x - round(x)) < tol
+  
+  expect_true(is.wholenumber(r1 * sims))
+  
+  
+  # Manually build an output with known proportion of extinctions
+  res2 <- res[c(1, 2, 1, 2), ]
+  res2$sim <- c(1, 1, 2, 2)
+  
+  # enforce that second sim did not have cases in final week.
+  #   I am ignoring cases_per_gen becayse I think this function works on weekly cases.
+  res2$weekly_cases[4] <- 0
+  res2$cumulative[4] <- res2$cumulative[3]
+  
+  # enforce that first sim has cases both weeks.
+  #   I am ignoring cases_per_gen becayse I think this function works on weekly cases.
+  res2$weekly_cases[1:2] <- c(1, 1)
+  res2$cumulative[1:2] <- c(1, 2)
+  
+  r2 <- extinct_prob(res2, cap_cases = cap, week_range = 1)
+  expect_equal(r2, 0.5)
+  
+  
+  
+  # Run some sims with almost certain outputs
+  
+  
+})
+
+
+
+
+test_that('extinct_prob week_range argument works', {
+  
+  
+  set.seed(12341240)
+  cap <- 100
+  sims <- 2
+  res <- scenario_sim(n.sim = 2,
+                      num.initial.cases = 5,
+                      cap_max_days = 100,
+                      cap_cases = cap,
+                      r0isolated = 0,
+                      r0community = 2.5,
+                      disp.iso = 1,
+                      disp.com = 0.16,
+                      k = 0.7,
+                      delay_shape = 2.5,
+                      delay_scale = 5,
+                      prop.asym = 0,
+                      prop.ascertain = 0)
+  
+  
+  # Manually build an output with known proportion of extinctions
+  res2 <- res[c(1, 2, 1, 2), ]
+  res2$sim <- c(1, 1, 2, 2)
+  
+  # enforce that second sim did not have cases in final week.
+  #   I am ignoring cases_per_gen becayse I think this function works on weekly cases.
+  res2$weekly_cases[4] <- 0
+  res2$cumulative[4] <- res2$cumulative[3]
+  
+  # enforce that first sim has cases both weeks.
+  #   I am ignoring cases_per_gen becayse I think this function works on weekly cases.
+  res2$weekly_cases[1:2] <- c(1, 1)
+  res2$cumulative[1:2] <- c(1, 2)
+  
+  r2 <- extinct_prob(res2, cap_cases = cap, week_range = 1)
+  expect_equal(r2, 0.5)
+
+  
+  # Now add a week and test week_range = 1:2
+  # Simple case of cases in week 1 and 2
+  res3 <- res[c(1, 2, 3), ]
+  res3$weekly_cases[1:3] <- c(1, 1, 1)
+  res3$cumulative[1:3] <- c(1, 2, 3)
+  r3 <- extinct_prob(res3, cap_cases = cap, week_range = 1:2)
+  expect_equal(r3, 0)
+  
+  
+  # Simple case of no cases in week 1 or 2
+  res4 <- res[c(1, 2, 3), ]
+  res4$weekly_cases[1:3] <- c(1, 0, 0)
+  res4$cumulative[1:3] <- c(1, 1, 1)
+  r4 <- extinct_prob(res4, cap_cases = cap, week_range = 1:2)
+  expect_equal(r4, 1)
+  
+  
+  
+  # Case of cases in week 1 but not 2 (by the definition used in this function this is not an extinction)
+  #   test here that week_range 1:2 says extintion by week_range 2 does not.
+  res5 <- res[c(1, 2, 3), ]
+  res5$weekly_cases[1:3] <- c(1, 1, 0)
+  res5$cumulative[1:3] <- c(1, 2, 2)
+  r5 <- extinct_prob(res5, cap_cases = cap, week_range = 1:2)
+  expect_equal(r5, 0)
+  
+  
+  
+  # Case of cases in week 2 but not 1 (by all sensible definitions is not an extinction)
+  #   test here that week_range 1:2 says extintion and week_range 2 does as well.
+  res6 <- res[c(1, 2, 3), ]
+  res6$weekly_cases[1:3] <- c(1, 0, 1)
+  res6$cumulative[1:3] <- c(1, 1, 2)
+  r6 <- extinct_prob(res6, cap_cases = cap, week_range = 1:2)
+  expect_equal(r6, 0)
+  
+    
+})
+
+
