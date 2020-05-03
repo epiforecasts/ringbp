64dee6d5dd5484dde8254908257ba79dcaf63e77
Tim Lucas <timcdlucas@gmail.com>
2020-05-03 19:19:20 +0100

    Get ready next large run with adherence and contact tracing delays.

--- inst/scripts/lucas_davis_results_20-05-02.R
+++ inst/scripts/lucas_davis_results_20-05-02.R
@@ -10,6 +10,13 @@
 #'geometry: margin=0.5in
 #'---
 
+#' # Major model update 1
+#' 
+#' - Quarentine now applies to asymptomatics
+#' - Examine delay for contact tracing
+#' - Change onset delay to either a 1 day delay or non-adherence.
+#' - People that are quarentined have adherence of 1.
+
 #+setup, echo = TRUE, cache = FALSE
 
 knitr::opts_chunk$set(cache = TRUE, fig.width = 8, fig.height = 5, cache.lazy = FALSE)
@@ -26,13 +33,10 @@ devtools::load_all()
 
 git2r::revparse_single('.',"HEAD")$sha
 
-# Make the log file
-#logs <- file.path("log_lucas.txt")
-#con <- file(logs, open = "wt")
-# # Send Output to log
-#sink(con)
-#sink(con, type = "message")
 
+#' Delay shape is adherence probability
+#' 
+#' Cap cases was chosen in a seperate analysis (choose_cap.R or something.)
 
 #+ create_parameters
 
@@ -41,9 +45,9 @@ no.samples <- 10
 scenarios <- tidyr::expand_grid(
   ## Put parameters that are grouped by disease into this data.frame
   delay_group = list(tibble::tibble(
-    delay = c("SARS"),
-    delay_shape = c(1.651524),
-    delay_scale = c(4.287786)
+    delay = c("Adherence"),
+    delay_shape = c(0.6, 0.9, 1),
+    delay_scale = 1
   )),
   inc_meanlog = 1.434065,
   inc_sdlog = 0.6612,
@@ -53,14 +57,17 @@ scenarios <- tidyr::expand_grid(
   min_quar_delay = 1,
   max_quar_delay = c(1, 4, 8),
   index_R0 = c(1.1, 1.6, 2),
-  prop.asym = c(0.2,0.4,0.5),
-  control_effectiveness = seq(0, 1, 0.2),
+  prop.asym = c(0.3,0.4,0.5),
+  control_effectiveness = seq(0.2, 1, 0.2),
   num.initial.cases = c(5)) %>%
   tidyr::unnest("delay_group") %>%
   dplyr::mutate(scenario = 1:dplyr::n())
 
-cap_cases <- 200
-max_days <- 300
+cap_cases <- 100
+max_days <- 50
+
+
+
 ## Parameterise fixed paramters
 sim_with_params <- purrr::partial(ringbp::scenario_sim,
                                   cap_max_days = max_days,
@@ -71,16 +78,11 @@ sim_with_params <- purrr::partial(ringbp::scenario_sim,
                                   quarantine = TRUE)
 
 
-#+ setup_multicore, cache = FALSE
-## Set up multicore if using see ?future::plan for details
-## Use the workers argument to control the number of cores used.
-#future::plan("multiprocess")
-
 
 #+ full_run
 
 tic()
-## Run paramter sweep
+## Run parameter sweep
 sweep_results <- ringbp::parameter_sweep(scenarios,
                                          sim_fn = sim_with_params,
                                          samples = no.samples,
@@ -91,10 +93,10 @@ toc()
 
 # #+ writeout
 
-saveRDS(sweep_results, file = "../../data-raw/lucas_quarentine_res.rds")
+saveRDS(sweep_results, file = "../../data-raw/lucas_davis_res.rds")
 
 
-
+#' Panel A is now redundant and has been replaced with an adherence probability.
 #+ plots1
 
 ringbp::make_figure_2()
@@ -116,29 +118,31 @@ res <- sweep_results %>%
 
 
 res %>% 
-  filter(delay == 'SARS') %>% 
-  mutate(prop.asym = factor(prop.asym, labels = c('asympt = 20%', '40%', '50%', '70%'))) %>%
+  filter(max_quar_delay == 1) %>% 
+  mutate(prop.asym = factor(prop.asym, labels = c('asympt = 30%', '50%', '70%'))) %>%
+  mutate(adherence = factor(delay_shape, labels = c('adhere = 60%', '90%', '100%'))) %>%
   mutate(index_R0 = factor(index_R0)) %>% 
   ggplot(aes(control_effectiveness, 1 - pext, colour = index_R0)) +
   geom_line() + 
   geom_point() + 
-  facet_grid(~ prop.asym) +
-  ggtitle('SARS') +
+  facet_grid(adherence ~ prop.asym) +
+  ggtitle('Contact trace delay is 1') +
   ylab('Prob. large outbreak')
 
 
-#+ plots_wuhan, eval = FALSE, cache = FALSE, fig.height = 5, fig.width = 9
 
 res %>% 
-  filter(delay == 'Wuhan') %>% 
-  mutate(prop.asym = factor(prop.asym, labels = c('asympt = 20%', '40%', '50%', '70%'))) %>%
+  filter(delay_shape == 1) %>% 
+  mutate(prop.asym = factor(prop.asym, labels = c('asympt = 30%', '50%', '70%'))) %>%
+  mutate(max_quar_delay = factor(max_quar_delay, labels = c('1 day trace delay', '4 days', '8 days'))) %>%
   mutate(index_R0 = factor(index_R0)) %>% 
   ggplot(aes(control_effectiveness, 1 - pext, colour = index_R0)) +
   geom_line() + 
   geom_point() + 
-  facet_grid(~ prop.asym) +
-  ggtitle('Wuhan')+
+  facet_grid(max_quar_delay ~ prop.asym) +
+  ggtitle('Adherence is 90%') +
   ylab('Prob. large outbreak')
+
 
 
 
@@ -149,9 +153,9 @@ week_range <- 40:42
 
 sweep_results2 <- 
   sweep_results %>% 
-  filter(delay == 'SARS') %>% 
-  filter(prop.asym == 0.4) %>% 
-  filter(control_effectiveness > 0.3)
+  filter(prop.asym == 0.4,
+         control_effectiveness > 0.3,
+         delay_shape == 0.9)
 
 for(i in seq_len(nrow(sweep_results2))){
   print(i)
@@ -172,7 +176,8 @@ for(i in seq_len(nrow(sweep_results2))){
   tmp <- 
     tmp %>% 
     mutate(index_R0 = sweep_results2$index_R0[i],
-           control_effectiveness = sweep_results2$control_effectiveness[i])
+           control_effectiveness = sweep_results2$control_effectiveness[i],
+           max_quar_delay = sweep_results2$max_quar_delay[i])
   
   res2[[i]] <- tmp              
 }
@@ -198,11 +203,12 @@ res2 <- do.call(rbind, res2)
 total_cumulative_distr <- 
   res2 %>% 
   mutate(total = ifelse(total > 2000, 2000, total)) %>% 
-  group_by(index_R0, control_effectiveness) %>% 
+  group_by(index_R0, control_effectiveness, max_quar_delay) %>% 
   do(res = tibble(cumdistr = nrow(.) * ecdf(.$total)(4:2000),
                   total = 4:2000,
                   outbreaks = nrow(.) - sum(.$extinct),
                   runs = nrow(.),
+                  max_quar_delay = .$max_quar_delay[1],
                   index_R0 = .$index_R0[1],
                   control_effectiveness = .$control_effectiveness[1],
                   poutbreak = (outbreaks) / (runs - cumdistr))) 
@@ -210,6 +216,7 @@ total_cumulative_distr <-
 
 total_cumulative_distr <- do.call(rbind, total_cumulative_distr$res) %>%
   mutate(index_R0 = factor(index_R0, labels = c('R0 = 1.1', '1.6', '2'))) %>% 
+  mutate(max_quar_delay = factor(max_quar_delay, labels = c('1 day trace delay', '4 days', '8 days'))) %>%
   filter(outbreaks != 0)
 
 
@@ -217,7 +224,7 @@ ggplot(total_cumulative_distr,
        aes(total, poutbreak, colour = factor(control_effectiveness), group = factor(control_effectiveness))) + 
   geom_line() + 
   xlim(0, 1000) +
-  facet_wrap(~ factor(index_R0), scale = 'free_y') +
+  facet_wrap(max_quar_delay ~ index_R0, scale = 'free_y') +
   ylab('Prob. large outbreak') + 
   guides(colour=guide_legend(title="Prop. Traced"))
 
@@ -226,12 +233,13 @@ ggplot(total_cumulative_distr,
 
 total_cumulative_distr <- 
   res2 %>% 
-  group_by(index_R0, control_effectiveness) %>% 
+  group_by(index_R0, control_effectiveness, max_quar_delay) %>% 
   do(res = tibble(cumdistr = sum(.$extinct) * ecdf(.$max_weekly[.$extinct == 1])(1:max(.$max_weekly)),
                   max_max_weekly = max(.$max_weekly),
                   max_weekly = 1:max(.$max_weekly),
                   extinct = sum(.$extinct),
                   outbreaks = nrow(.) - sum(.$extinct),
+                  max_quar_delay = .$max_quar_delay[1],
                   runs = nrow(.),
                   index_R0 = .$index_R0[1],
                   control_effectiveness = .$control_effectiveness[1],
@@ -241,13 +249,15 @@ total_cumulative_distr <-
 total_cumulative_distr <- 
   do.call(rbind, total_cumulative_distr$res) %>%
   filter(poutbreak < 1) %>%
-  mutate(index_R0 = factor(index_R0, labels = c('R0 = 1.1', '1.6', '2')))
+  mutate(index_R0 = factor(index_R0, labels = c('R0 = 1.1', '1.6', '2'))) %>%
+  mutate(max_quar_delay = factor(max_quar_delay, labels = c('1 day trace delay', '4 days', '8 days')))
+  
 
 
 ggplot(total_cumulative_distr, 
        aes(max_weekly, poutbreak, colour = factor(control_effectiveness), group = factor(control_effectiveness))) + 
   geom_line() + 
-  facet_wrap(~ factor(index_R0), scale = 'free_x') +
+  facet_wrap(max_quar_delay ~ index_R0, scale = 'free_x') +
   ylab('Prob. large outbreak') + 
   guides(colour=guide_legend(title="Prop. Traced"))
 
