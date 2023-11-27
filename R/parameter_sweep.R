0bbb5721e2fc57503fd3dff501cc14a29ab27525
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2023-11-27 15:02:40 +0000

    update deprecated future_options

--- R/parameter_sweep.R
+++ R/parameter_sweep.R
@@ -99,7 +99,7 @@ parameter_sweep <- function(scenarios = NULL, samples = 1,
                prop.asym = .$prop.asym
       )[[1]],
       .progress = show_progress,
-      .options = furrr::future_options(scheduling = 20)
+      .options = furrr::furrr_options(scheduling = 20)
     )) %>%
     tidyr::unnest(cols = "data")
 
