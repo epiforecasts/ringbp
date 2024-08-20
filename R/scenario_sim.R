d1bfaa2265ea30b2063b71fb0c5a90c875fe92d6
Joshua Lambert <joshua.lambert@lshtm.ac.uk>
2024-08-20 11:57:32 +0100

    linting scenario_sim

--- R/scenario_sim.R
+++ R/scenario_sim.R
@@ -13,13 +13,14 @@
 #' @importFrom stats quantile
 #' @importFrom stats rweibull
 #' @importFrom utils data
-#' @return A data.table object returning the results for multiple simulations using
-#' the same set of parameters. The table has columns
+#' @return A data.table object returning the results for multiple simulations
+#' using the same set of parameters. The table has columns
 #' * week: The week in the simulation.
 #' * weekly_cases: The number of new cases that week.
 #' * cumulative: The cumulative cases.
 #' * effective_r0: The effective reproduction rate for the whole simulation
-#' * cases_per_gen: A list column with the cases per generation. This is repeated each row.
+#' * cases_per_gen: A list column with the cases per generation. This is
+#' repeated each row.
 #' * sim: Index column for which simulation.
 #' @export
 #'
@@ -45,30 +46,35 @@ scenario_sim <- function(n.sim, prop.ascertain, cap_max_days, cap_cases,
                          delay_shape, delay_scale, num.initial.cases, prop.asym,
                          quarantine, r0subclin = NULL, disp.subclin = NULL) {
 
-  # Set infectiousness of subclinical cases to be equal to clinical cases unless specified otherwise
-  if(is.null(r0subclin)) {
+  # Set infectiousness of subclinical cases to be equal to clinical cases
+  # unless specified otherwise
+  if (is.null(r0subclin)) {
     r0subclin <- r0community
   }
 
-  if(is.null(disp.subclin)) {
+  if (is.null(disp.subclin)) {
     disp.subclin <- disp.com
   }
   # Run n.sim number of model runs and put them all together in a big data.frame
-  res <- purrr::map(.x = 1:n.sim, ~ outbreak_model(num.initial.cases = num.initial.cases,
-                                             prop.ascertain = prop.ascertain,
-                                             cap_max_days = cap_max_days,
-                                             cap_cases = cap_cases,
-                                             r0isolated = r0isolated,
-                                             r0community = r0community,
-                                             r0subclin = r0subclin,
-                                             disp.subclin = disp.subclin,
-                                             disp.iso = disp.iso,
-                                             disp.com = disp.com,
-                                             delay_shape = delay_shape,
-                                             delay_scale = delay_scale,
-                                             k = k,
-                                             prop.asym = prop.asym,
-                                             quarantine = quarantine))
+  res <- purrr::map(
+    .x = 1:n.sim, ~ outbreak_model(
+      num.initial.cases = num.initial.cases,
+      prop.ascertain = prop.ascertain,
+      cap_max_days = cap_max_days,
+      cap_cases = cap_cases,
+      r0isolated = r0isolated,
+      r0community = r0community,
+      r0subclin = r0subclin,
+      disp.subclin = disp.subclin,
+      disp.iso = disp.iso,
+      disp.com = disp.com,
+      delay_shape = delay_shape,
+      delay_scale = delay_scale,
+      k = k,
+      prop.asym = prop.asym,
+      quarantine = quarantine
+    )
+  )
 
 
   # bind output together and add simulation index
@@ -78,13 +84,13 @@ scenario_sim <- function(n.sim, prop.ascertain, cap_max_days, cap_cases,
 }
 
 
-utils::globalVariables(c(".", ".N", ":=", "asym", "control_effectiveness", "cumulative", "data", "delay",
-                         "disp",  "exposure", "extinct", "full_join", "incubfn_sample",
-                         "index_R0", "infector_iso_time", "iqr_lower", "iqr_upper", "isolated", "isolated_time",
-                         "k", "label_extinct", "lower", "max_weekly_cases", "median_eff_r0", "missed",
-                         "new_cases", "num.initial.cases", "onset", "pext", "prob_extinct", "prop.asym",
-                         "r0", "rweibull", "samp", "samples", "scenario", "sim", "sims", "theta", "upper", "value",
-                         "week", "weekly_cases", "x", "y", "y0", "y100", "y25", "y50", "y75"))
-                         
-
-
+utils::globalVariables(
+  c(".", ".N", ":=", "asym", "control_effectiveness", "cumulative", "data",
+    "delay", "disp",  "exposure", "extinct", "full_join", "incubfn_sample",
+    "index_R0", "infector_iso_time", "iqr_lower", "iqr_upper", "isolated",
+    "isolated_time", "k", "label_extinct", "lower", "max_weekly_cases",
+    "median_eff_r0", "missed", "new_cases", "num.initial.cases", "onset",
+    "pext", "prob_extinct", "prop.asym", "r0", "rweibull", "samp", "samples",
+    "scenario", "sim", "sims", "theta", "upper", "value", "week",
+    "weekly_cases", "x", "y", "y0", "y100", "y25", "y50", "y75")
+)
