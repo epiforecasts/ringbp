73516010cf19538cfa6a2f7af790971f13519445
Joshua Lambert <joshua.lambert@lshtm.ac.uk>
2025-06-11 17:42:10 +0100

    add check_dist_func function and call in check_outbreak_input, fixes #91

--- R/checking.R
+++ R/checking.R
@@ -23,9 +23,9 @@ check_outbreak_input <- function(func = c("parameters", "control")) {
     checkmate::assert_number(args$disp_community, lower = 0, finite = TRUE)
     checkmate::assert_number(args$disp_isolated, lower = 0, finite = TRUE)
     checkmate::assert_number(args$disp_asymptomatic, lower = 0, finite = TRUE)
-    checkmate::assert_function(args$incubation_period)
+    check_dist_func(args$incubation_period, dist_name = "incubation_period")
     checkmate::assert_number(args$prop_presymptomatic, lower = 0, upper = 1)
-    checkmate::assert_function(args$onset_to_isolation)
+    check_dist_func(args$onset_to_isolation, dist_name = "onset_to_isolation")
     checkmate::assert_number(args$prop_ascertain, lower = 0, upper = 1)
     checkmate::assert_number(args$prop_asymptomatic, lower = 0, upper = 1)
     checkmate::assert_logical(args$quarantine, any.missing = FALSE, len = 1)
@@ -38,3 +38,43 @@ check_outbreak_input <- function(func = c("parameters", "control")) {
 
   return(TRUE)
 }
+
+#' Check a distribution function returns valid output and has the correct number
+#' of arguments
+#'
+#' @param func a [function].
+#' @param dist_name a `character` string: the name of the distribution function
+#'   being passed (e.g. `"incubation_period"`)
+#' @param n_req_args a single `numeric`: the number of required arguments
+#'
+#' @return `TRUE` if all the checks pass or an error is thrown if the
+#'   distribution function is invalid.
+#' @keywords internal
+check_dist_func <- function(func,
+                            dist_name,
+                            n_req_args = 1) {
+
+  checkmate::assert_function(func)
+  checkmate::assert_count(n_req_args, positive = TRUE)
+  # using formals(args(fn)) to allow checking args of builtin primitives
+  # for which formals(fn) would return NULL and cause the check to error
+  # errors non-informatively for specials such as `if`
+  valid_func <- checkmate::test_function(func) &&
+    sum(mapply(function(x, y) { # nolint undesirable function
+      is.name(x) && y != "..."
+    }, formals(args(func)), names(formals(args(func))))) == n_req_args &&
+    # incubation_period and onset_to_isolation are strictly positive
+    checkmate::test_numeric(
+      func(1e5), lower = 0, finite = TRUE, any.missing = FALSE, len = 1e5
+    )
+
+  if (!valid_func) {
+    stop(
+      dist_name, " must be a function with ", n_req_args, " argument(s) that ",
+      "returns positive numbers."
+    )
+  }
+
+  return(TRUE)
+}
+
