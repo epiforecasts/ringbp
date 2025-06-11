017a79fc11c0632de8264bc825e1b8d7cef50aad
Joshua Lambert <joshua.lambert@lshtm.ac.uk>
2025-06-11 12:03:26 +0100

    update check_outbreak_input to check parameters and control lists, fixes #125

--- R/checking.R
+++ R/checking.R
@@ -9,47 +9,31 @@
 #' @return `TRUE` if all the checks pass or an error thrown by a \pkg{checkmate}
 #' `assert_*()` function if one or more of the inputs is invalid.
 #' @keywords internal
-check_outbreak_input <- function() {
-  ## get name of the calling function as a character string
-  func <- deparse(as.list(sys.call(-1))[[1]])
-  func <- gsub(pattern = "ringbp::", replacement = "", x = func)
-  func <- match.arg(func, choices = c(
-    "outbreak_setup", "outbreak_step", "outbreak_model", "scenario_sim"
-  ))
+check_outbreak_input <- function(func = c("parameters", "control")) {
+
+  func <- match.arg(func)
+
   args <- as.list(parent.frame())
 
-  checkmate::assert_function(args$incubation_period)
-  checkmate::assert_function(args$onset_to_isolation)
-  checkmate::assert_number(args$prop_asymptomatic, lower = 0, upper = 1)
-
-  if (func %in% c("outbreak_setup", "outbreak_model", "scenario_sim")) {
+  if (func == "parameters") {
     checkmate::assert_number(args$initial_cases, lower = 1, finite = TRUE)
-  }
-
-  if (func %in% c("outbreak_step", "outbreak_model", "scenario_sim")) {
     checkmate::assert_number(args$r0_community, lower = 0, finite = TRUE)
     checkmate::assert_number(args$r0_isolated, lower = 0, finite = TRUE)
     checkmate::assert_number(args$r0_asymptomatic, lower = 0, finite = TRUE)
     checkmate::assert_number(args$disp_community, lower = 0, finite = TRUE)
     checkmate::assert_number(args$disp_isolated, lower = 0, finite = TRUE)
     checkmate::assert_number(args$disp_asymptomatic, lower = 0, finite = TRUE)
+    checkmate::assert_function(args$incubation_period)
+    checkmate::assert_number(args$prop_presymptomatic, lower = 0, upper = 1)
+    checkmate::assert_function(args$onset_to_isolation)
     checkmate::assert_number(args$prop_ascertain, lower = 0, upper = 1)
+    checkmate::assert_number(args$prop_asymptomatic, lower = 0, upper = 1)
     checkmate::assert_logical(args$quarantine, any.missing = FALSE, len = 1)
   }
 
-  if (func %in% c("outbreak_model", "scenario_sim")) {
-    checkmate::assert_number(args$prop_presymptomatic, lower = 0, upper = 1)
+  if (func == "control") {
     checkmate::assert_int(args$cap_max_days, lower = 1)
     checkmate::assert_int(args$cap_cases, lower = 1)
-  }
-
-  if (func == "outbreak_step") {
-    checkmate::assert_data_table(args$case_data)
-    checkmate::assert_number(args$alpha, finite = TRUE)
-  }
-
-  if (func == "scenario_sim") {
-    checkmate::assert_number(args$n, lower = 1, finite = TRUE)
   }
 
   return(TRUE)
