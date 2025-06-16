c9aa13d37be4690684fc73c74ebb0a4e2a09e51e
Joshua Lambert <joshua.lambert@lshtm.ac.uk>
2025-06-16 11:21:08 +0100

    remove check_outbreak_input now *_opts function check input

--- R/checking.R
+++ R/checking.R
@@ -1,46 +1,3 @@
-#' Check the function arguments of the outbreak simulation functions
-#'
-#' @details If more than one argument is invalid, only a single error is
-#'   thrown with the first invalid argument encountered.
-#'
-#'   Arguments being checked are taken from the parent environment
-#'   ([parent.frame()]) rather than passed via named arguments.
-#'
-#' @param parent_func a `character` string: the name of the calling function.
-#'
-#' @return `TRUE` if all the checks pass or an error thrown by a \pkg{checkmate}
-#' `assert_*()` function if one or more of the inputs is invalid.
-#' @keywords internal
-check_outbreak_input <- function(parent_func = c("parameters", "control")) {
-
-  parent_func <- match.arg(parent_func)
-
-  args <- as.list(parent.frame())
-
-  if (parent_func == "parameters") {
-    checkmate::assert_number(args$initial_cases, lower = 1, finite = TRUE)
-    checkmate::assert_number(args$r0_community, lower = 0, finite = TRUE)
-    checkmate::assert_number(args$r0_isolated, lower = 0, finite = TRUE)
-    checkmate::assert_number(args$r0_asymptomatic, lower = 0, finite = TRUE)
-    checkmate::assert_number(args$disp_community, lower = 0, finite = TRUE)
-    checkmate::assert_number(args$disp_isolated, lower = 0, finite = TRUE)
-    checkmate::assert_number(args$disp_asymptomatic, lower = 0, finite = TRUE)
-    check_dist_func(args$incubation_period, dist_name = "incubation_period")
-    checkmate::assert_number(args$prop_presymptomatic, lower = 0, upper = 1)
-    check_dist_func(args$onset_to_isolation, dist_name = "onset_to_isolation")
-    checkmate::assert_number(args$prop_ascertain, lower = 0, upper = 1)
-    checkmate::assert_number(args$prop_asymptomatic, lower = 0, upper = 1)
-    checkmate::assert_logical(args$quarantine, any.missing = FALSE, len = 1)
-  }
-
-  if (parent_func == "control") {
-    checkmate::assert_int(args$cap_max_days, lower = 1)
-    checkmate::assert_int(args$cap_cases, lower = 1)
-  }
-
-  return(TRUE)
-}
-
 #' Check a distribution function returns valid output and has the correct number
 #' of arguments
 #'
@@ -65,7 +22,8 @@ check_dist_func <- function(func,
     sum(mapply(function(x, y) { # nolint undesirable function
       is.name(x) && y != "..."
     }, formals(args(func)), names(formals(args(func))))) == n_req_args &&
-    # incubation_period and onset_to_isolation are strictly positive
+    # offspring distribution, incubation_period and onset_to_isolation are
+    # strictly positive
     checkmate::test_numeric(
       func(1e5), lower = 0, finite = TRUE, any.missing = FALSE, len = 1e5
     )
@@ -79,4 +37,3 @@ check_dist_func <- function(func,
 
   return(TRUE)
 }
-
