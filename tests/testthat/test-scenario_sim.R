0e723654196f4ca9793de04c5ec5489e9f6c7eb2
Joshua Lambert <joshua.lambert@lshtm.ac.uk>
2025-09-17 14:29:46 +0100

    add scenario_sim test with dynamic seed and parameters

--- tests/testthat/test-scenario_sim.R
+++ tests/testthat/test-scenario_sim.R
@@ -33,3 +33,61 @@ test_that("scenario_sim runs as expected", {
   )
   expect_identical(unique(res$sim), 1:n_sim)
 })
+
+test_that("scenario_sim with dynamic seed and parameters runs as expected", {
+  seed <- as.integer(Sys.Date())
+  set.seed(seed)
+  n_sim <- 5
+  res <- scenario_sim(
+    n = n_sim,
+    initial_cases = sample(1:50, size = 1),
+    offspring = offspring_opts(
+      community = \(n) rnbinom(
+        n = n,
+        mu = runif(n = 1, min = 0.5, max = 3),
+        size = runif(n = 1, min = 0.1, max = 1000)
+      ),
+      isolated = \(n) rnbinom(
+        n = n,
+        mu = runif(n = 1, min = 0, max = 3),
+        size = runif(n = 1, min = 0.1, max = 1000)
+      ),
+      asymptomatic = \(n) rnbinom(
+        n = n,
+        mu = runif(n = 1, min = 0.5, max = 3),
+        size = runif(n = 1, min = 0.1, max = 1000)
+      )
+    ),
+    delays = delay_opts(
+      incubation_period = \(n) rweibull(
+        n = n,
+        shape = runif(n = 1, min = 2, max = 4),
+        scale = runif(n = 1, min = 2, max = 6)
+      ),
+      onset_to_isolation = \(n) rweibull(
+        n = n,
+        shape = runif(n = 1, min = 2, max = 4),
+        scale = runif(n = 1, min = 2, max = 6)
+      )
+    ),
+    event_probs = event_prob_opts(
+      asymptomatic = runif(n = 1, min = 0, max = 1),
+      presymptomatic_transmission = runif(n = 1, min = 0, max = 1),
+      symptomatic_ascertained = runif(n = 1, min = 0, max = 1)
+    ),
+    interventions = intervention_opts(
+      quarantine = sample(c(TRUE, FALSE), size = 1)
+    ),
+    sim = sim_opts(
+      cap_max_days = sample(100:1000, size = 1),
+      cap_cases = sample(100:10000, size = 1)
+    )
+  )
+  expect_s3_class(res, class = c("data.table", "data.frame"), exact = TRUE)
+  expect_identical(
+    vapply(res, class, FUN.VALUE = character(1)),
+    c(sim = "integer", week = "numeric", weekly_cases = "numeric",
+      cumulative = "numeric", effective_r0 = "numeric", cases_per_gen = "list")
+  )
+  expect_identical(unique(res$sim), 1:n_sim)
+})
